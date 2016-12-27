8958f866b39928e5b8547721f2789293b8d2b10a
Saeed Jassani <saeedjassani@gmail.com>
2016-12-27 21:23:21 +0530

    Config changes

---
+++ app/src/main/java/com/github/expandablelayoutsample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.github.expandablelayoutsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.github.saeedjassani.ExpandableLayout;
+import com.github.saeedjassani.ExpandableLayoutListView;
+
+
+public class MainActivity extends Activity {
+
+	private final String[] array = {"Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.view_row, R.id.header_text, array);
+		final ExpandableLayoutListView expandableLayoutListView = (ExpandableLayoutListView) findViewById(R.id.listview);
+
+		expandableLayoutListView.setAdapter(arrayAdapter);
+
+		final ExpandableLayout expandableLayout = (ExpandableLayout) findViewById(R.id.first);
+		expandableLayout.setOnHeaderClickListener(new ExpandableLayout.OnHeaderClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (expandableLayout.isOpened()) {
+					Toast.makeText(MainActivity.this, "Layout collapsed", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(MainActivity.this, "Layout expanded", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+	}
+
+}
